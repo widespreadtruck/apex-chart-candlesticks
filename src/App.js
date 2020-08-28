0a4f774e2acca3c653227b4e782edbd5f9e32217
Nikita Guliaev <gulnikita63@gmail.com>
2020-08-28 17:54:35 -0400

    candle - class comp

--- src/App.js
+++ src/App.js
@@ -1,26 +1,85 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import ReactApexChart from "react-apexcharts";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      series: [],
+      options: {
+        chart: {
+          type: 'candlestick',
+          height: 350
+        },
+        title: {
+          text: 'CandleStick Chart',
+          align: 'left'
+        },
+        xaxis: {
+          type: 'datetime'
+        },
+        yaxis: {
+          tooltip: {
+            enabled: true
+          }
+        }
+      },
+    };
+
+  };
+
+
+  getCoinData = async (numOfDaysBack) => {
+    const url = `https://api.coingecko.com/api/v3/coins/bitcoin/ohlc?vs_currency=usd&days=${numOfDaysBack}`;
+    try {
+      const coinResponse = await fetch(url);
+      const data = await coinResponse.json();
+      this.prepData(data);
+    } catch (e) {
+      prompt(e);
+    }
+  };
+  
+  
+  prepData = (data) => {
+    let dataArr = [{
+      data: []
+    }];
+    for (let i = 0; i < data.length; i++) {
+      const date = new Date(data[i].[0]);
+      const singlePrice = data[i];
+      const prices = [singlePrice.[1], singlePrice.[2], singlePrice.[3], singlePrice.[4]];
+  
+      dataArr[0].["data"].push({
+          x: date,
+          y: prices,
+        }
+      );
+
+    };
+    this.setState({
+      series: dataArr,
+    });
+  };
+
+  componentDidMount() {
+    this.getCoinData(30);
+
+  }
+
+
+
+  render() {
+    return (
+      <div id="chart">
+        <ReactApexChart
+          options={this.state.options}
+          series={this.state.series}
+          type="candlestick"
+          height={350} />
+      </div>
+    )
+  };
 }
-
 export default App;
