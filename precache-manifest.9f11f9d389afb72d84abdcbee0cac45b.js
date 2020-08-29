1917257de7ba3b5f1ec2f9dafd1d764970b7af37
Nikita Guliaev <gulnikita63@gmail.com>
2020-08-29 19:19:33 -0400

    Updates

---
+++ precache-manifest.9f11f9d389afb72d84abdcbee0cac45b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f6d5dcc379f4ccb59b9c1738b7b046fa",
+    "url": "/apex-chart-candlesticks/index.html"
+  },
+  {
+    "revision": "ff239b5a401bc1c98316",
+    "url": "/apex-chart-candlesticks/static/js/2.3039479a.chunk.js"
+  },
+  {
+    "revision": "4f44a17bb08e78ed1b92485defc539ac",
+    "url": "/apex-chart-candlesticks/static/js/2.3039479a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "341e41ccc17388934d02",
+    "url": "/apex-chart-candlesticks/static/js/main.9e891def.chunk.js"
+  },
+  {
+    "revision": "a7189ff931f627746554",
+    "url": "/apex-chart-candlesticks/static/js/runtime-main.8fdf306b.js"
+  }
+]);
